ab57215b819e9424806910e3df94fa588c8f9232
Kevin Barrazueta <kabarrazueta@utpl.edu.ec>
2022-01-23 14:16:27 -0500

    primer commit

--- Ejercicios/src/ejercicios/Problema00.java
+++ Ejercicios/src/ejercicios/Problema00.java
@@ -13,15 +13,20 @@ public class Problema00 {
 
     public static void main(String[] args) {
         int[] datos = {10, 2, 4, 1};
-        System.out.printf("%d\n", misterio(datos, datos.length));
+        System.out.printf("%d\n", misterio(datos, datos.length)); 
+        // envia el arreglo y el tamaño del arreglo
     }
 
     public static int misterio(int[] arreglo, int tamanio) {
-
-        if (tamanio == 1) {
+        // el proceso realiza la suma de los elementos del arreglo
+        if (tamanio == 1) { // si el arreglo tiene tamaño de 1 presentara el 
+                            // unico valor
+            
             return arreglo[0];
         } else {
             return arreglo[tamanio - 1] + misterio(arreglo, tamanio - 1);
+            // retorna el arreglo en la posicion x - 1 y le suma la funcion 
+            // enviandole el arreglo y la posicion
         }
     }
 
