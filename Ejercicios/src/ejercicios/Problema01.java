22acaf1c3a984fbc0c91c229e81db6ed74c0d1a1
Kevin Barrazueta <kabarrazueta@utpl.edu.ec>
2022-01-23 14:09:13 -0500

    primer commit

--- Ejercicios/src/ejercicios/Problema01.java
+++ Ejercicios/src/ejercicios/Problema01.java
@@ -16,6 +16,23 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[][] datos = {{10, 2}, {1, 5}, {6, 2}, {7, 8}};
+        double[][] resultado = new double[4][2];
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                resultado[i][j] = factorial(datos[i][j]);
+                System.out.printf("%s valor factorial %s\n", datos[i][j],
+                        resultado[i][j]);
+            }
+        }
+
     }
-    
+
+    public static double factorial(double numero) {
+        if (numero <= 1) {
+            return 1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
+    }
 }
--- Ejercicios/src/ejercicios/Problema04.java
+++ Ejercicios/src/ejercicios/Problema04.java
@@ -16,6 +16,37 @@ public class Problema04 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        double[] bases = {100, 90, 80, 70, 60};
+        double[] potencias = {2, 3, 2, 2, 1};
+        double[] resultado = new double[5];
+        for (int i = 0; i < bases.length; i++) {
+            resultado[i] = obtenerPotencia(bases[i], potencias[i]);
+            System.out.printf("%s elevado a %s es: %.2f\n",
+                    bases[i], 
+                    potencias[i],
+                    resultado[i]);
+        }
+        System.out.printf("La suma final es: %s\n", suma(resultado, resultado.length));
+
     }
-    
+
+    public static double obtenerPotencia(double base, double exponente) {
+
+        if (exponente == 1) {
+            return base;
+        } else {
+            return base * obtenerPotencia(base, exponente - 1);
+
+        }
+    }
+
+    public static double suma(double[] arreglo, int capacidad) {
+
+        if (capacidad == 1) {
+            return arreglo[0];
+        } else {
+            return arreglo[capacidad - 1] + suma(arreglo, capacidad - 1);
+        }
+    }
 }
